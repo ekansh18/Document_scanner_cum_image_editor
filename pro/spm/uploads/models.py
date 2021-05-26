a01cddf52dba9766670e111efb8524d6aa814d35
Ekansh <ekansh00@gmail.com>
2021-05-26 11:37:57 +0530

    complete

--- pro/spm/uploads/models.py
+++ pro/spm/uploads/models.py
@@ -25,7 +25,7 @@ class Upload(models.Model):
     action=models.CharField(max_length=50, choices=ACTION_CHOICES)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
-    sca=models.CharField(max_length=50, choices=SC_CHOICES, default='YES')
+    sca=models.CharField(max_length=50, choices=SC_CHOICES, default='NO')
     
     def __str__(self):
         return str(self.id)
